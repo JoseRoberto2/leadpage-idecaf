ca5dcdc4b00c510c8a77f7b241c1e663876c9c1d
jose roberto <robertojuniorluiz16@gmail.com>
2026-04-23 11:50:26 -0300

    alter deploy

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base: "/leadpage-idecaf/dist/",
+  base: "/leadpage-idecaf/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
